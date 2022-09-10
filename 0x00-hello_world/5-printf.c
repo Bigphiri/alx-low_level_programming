2005c4c332a9f0d46fcb23fea4cda2550a2966ff
Bigphiri <mankhambamwai@gmail.com>
2022-09-10 15:47:14 -0700

    printf

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * Return: Awalys 0 (Success)
+ *
+ * printf - displays content to the screen
+ */
+int main ()
+{
+
+	printf("%s", "wit ");
+
+	return (0);
+}
