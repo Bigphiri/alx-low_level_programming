b3c267689e150a1ccc0dc21135cba22b38288043
Bigphiri <mankhambamwai@gmail.com>
2022-09-11 13:32:42 -0700

    size

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -1,10 +1,27 @@
 #include <stdio.h>
 
+/**
+ * main - Entry point
+ *
+ * printf - Dispays output
+ *
+ * sizeof - Gives the size of a datatype
+ *
+ * Return: Always 0 (Success)
+ */
 int main(void)
 {
-	char x;
-	
-	printf("%s %d", "size of a char:", (int)sizeof(char));
-			
+	int intType;
+	char charType;
+	float floatType;
+	long longType;
+	long long longlongType;
+
+	/*prints the sizes of the data types*/
+	printf("size of a char: %zu byte(s)\n", sizeof(charType));
+	printf("size of an int: %zu byte(s)\n", sizeof(intType));
+	printf("size of a long int: %zu byte(s)\n", sizeof(longType));
+	printf("size of a long long: %zu byte(s)\n", sizeof(longlongType));
+	printf("size of a float: %zu byte(s)\n", sizeof(floatType));
 	return (0);
 }
