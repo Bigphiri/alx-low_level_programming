fcb9cb5b09c5b011593e676a5ab5e05f8f0acde8
Bigphiri <mankhambamwai@gmail.com>
2022-09-11 09:56:49 -0700

    puts update

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -11,7 +11,7 @@
  */
 int main(void)
 {
-	char my_string[60]="\"Programming is like building a multilingual puzzle\n";
+	char my_string[60] = "\"Programming is like building a multilingual puzzle\n";
 
 	puts(my_string);
 
